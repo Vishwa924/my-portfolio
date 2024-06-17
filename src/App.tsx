94e272d8ee3b342892b4e9dda969455c58a25aee
Vishwanathan M <msvishwa50@gmail.com>
2024-06-17 18:29:55 +0530

    source file

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="nav-menu">
+     <ul>
+      <li>Home</li>
+      <li>Skills</li>
+      <li>Projects</li>
+      <li>Education</li>
+      <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
